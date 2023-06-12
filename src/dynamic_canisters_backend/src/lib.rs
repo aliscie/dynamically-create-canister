298b4c774d257316d2ee47b2bae8d01e3050310a
aliscie <alihushamsci@icloud.com>
2023-06-12 17:28:20 +0300

    create a new canister from wasm

--- src/dynamic_canisters_backend/src/lib.rs
+++ src/dynamic_canisters_backend/src/lib.rs
@@ -1,308 +1,164 @@
+use std::cell::RefCell;
+
+use candid::Principal;
 use ic_cdk::api;
+use ic_cdk::api::call::CallResult;
+use ic_cdk::api::management_canister::main::CanisterSettings;
 use ic_cdk::export::candid::{
     candid_method, CandidType, check_prog, Deserialize, export_service, IDLProg, TypeEnv,
 };
-use std::cell::RefCell;
 
+#[derive(Default)]
+struct WalletWASMBytes(Option<serde_bytes::ByteBuf>);
 
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+/// The wallet (this canister's) name.
+#[derive(Default)]
+struct WalletName(pub(crate) Option<String>);
 
-#[derive(Clone, CandidType, Deserialize)]
-pub struct ChartTick {
-    timestamp: u64,
-    cycles: u64,
-}
 
 thread_local! {
-    static CHART_TICKS: RefCell<Vec<ChartTick>> = Default::default();
+    static WALLET_NAME: RefCell<WalletName> = Default::default();
+    static WALLET_WASM_BYTES: RefCell<WalletWASMBytes> = Default::default();
 }
 
-fn update_chart() {
-    let timestamp = api::time();
-    let cycles = api::canister_balance();
-    CHART_TICKS.with(|mut chart| chart.borrow_mut().push(ChartTick { timestamp, cycles }));
+
+#[candid_method(query)]
+#[ic_cdk::query]
+fn my_canister_id() -> String {
+    format!("{}", ic_cdk::id())
 }
 
-mod wallet {
-    use ic_cdk::export::candid::{
-        candid_method, CandidType, check_prog, Deserialize, export_service, IDLProg, TypeEnv,
-    };
-    // use ic_cdk::query;
-    use std::convert::TryInto;
 
-    use ic_cdk::*;
-    use ic_cdk::export::candid::{Nat};
-    use ic_cdk::export::Principal;
-    use super::*;
+#[derive(CandidType, Deserialize)]
+struct CreateResult {
+    canister_id: Principal,
+}
 
-    /***************************************************************************************************
-             * Cycle Management
-             **************************************************************************************************/
+#[derive(CandidType, Deserialize)]
+struct CanisterInstall {
+    mode: InstallMode,
+    canister_id: Principal,
+    #[serde(with = "serde_bytes")]
+    wasm_module: Vec<u8>,
+    arg: Vec<u8>,
+}
+
+
+#[derive(CandidType, Deserialize)]
+enum InstallMode {
+    #[serde(rename = "install")]
+    Install,
+    #[serde(rename = "reinstall")]
+    Reinstall,
+    #[serde(rename = "upgrade")]
+    Upgrade,
+}
+
+
+#[candid_method(update)]
+#[ic_cdk::update]
+async fn create_canister() -> String {
     #[derive(CandidType)]
-    struct BalanceResult<TCycles> {
-        amount: TCycles,
+    struct In {
+        settings: Option<CanisterSettings>,
     }
+    let settings = CanisterSettings {
+        controllers: Some(vec![ic_cdk::api::id()]),
+        compute_allocation: None,
+        memory_allocation: None,
+        freezing_threshold: None,
+    };
+    let in_arg = In {
+        settings: Some(settings),
+    };
 
-    #[derive(CandidType, Deserialize)]
-    struct SendCyclesArgs<TCycles> {
-        canister: Principal,
-        amount: TCycles,
-    }
-
-    /// Return the cycle balance of this canister.
-    // #[query(guard = "is_custodian_or_controller", name = "wallet_balance")]
-    #[candid_method(query)]
-    #[ic_cdk::query]
-    fn balance() -> BalanceResult<u64> {
-        BalanceResult {
-            amount: api::canister_balance128()
-                .try_into()
-                .expect("Balance exceeded a 64-bit value; call `wallet_balance128`"),
+    let (create_result, ): (CreateResult, ) = match api::call::call_with_payment128(
+        Principal::management_canister(),
+        "create_canister",
+        (in_arg, ),
+        400_000_000_000,
+    )
+        .await
+    {
+        Ok(x) => x,
+        Err((code, msg)) => {
+            return format!(
+                "An error happened during the call: {}: {}",
+                code as u8, msg
+            );
         }
-    }
+    };
 
-    // #[query(guard = "is_custodian_or_controller", name = "wallet_balance128")]
-    #[candid_method(query)]
-    #[ic_cdk::query]
-    fn balance128() -> BalanceResult<u128> {
-        BalanceResult {
-            amount: api::canister_balance128(),
+    // wasm install
+
+    let wasm_module = WALLET_WASM_BYTES.with(|wallet_bytes| match &wallet_bytes.borrow().0 {
+        Some(o) => o.clone().into_vec(),
+        None => {
+            ic_cdk::trap("No wasm module stored.");
         }
-    }
+    });
 
+    let install_config = CanisterInstall {
+        mode: InstallMode::Install,
+        canister_id: ic_cdk::api::id(),
+        wasm_module: wasm_module.clone(),
+        arg: b" ".to_vec(),
+    };
 
-    /***************************************************************************************************
-     * Managing Canister
-     **************************************************************************************************/
-    #[derive(CandidType, Clone, Deserialize)]
-    struct CanisterSettings {
-        // dfx versions <= 0.8.1 (or other wallet callers expecting version 0.1.0 of the wallet)
-        // will set a controller (or not) in the the `controller` field:
-        controller: Option<Principal>,
-
-        // dfx versions >= 0.8.2 will set 0 or more controllers here:
-        controllers: Option<Vec<Principal>>,
-
-        compute_allocation: Option<Nat>,
-        memory_allocation: Option<Nat>,
-        freezing_threshold: Option<Nat>,
-    }
-
-    #[derive(CandidType, Clone, Deserialize)]
-    struct CreateCanisterArgs<TCycles> {
-        cycles: TCycles,
-        settings: CanisterSettings,
-    }
-
-    #[derive(CandidType, Deserialize)]
-    struct UpdateSettingsArgs {
-        canister_id: Principal,
-        settings: CanisterSettings,
-    }
-
-    #[derive(CandidType, Deserialize)]
-    struct CreateResult {
-        canister_id: Principal,
-    }
-
-    // #[update(guard = "is_custodian_or_controller", name = "wallet_create_canister")]
-    #[candid_method(update)]
-    #[ic_cdk::update]
-    async fn create_canister(
-        CreateCanisterArgs { cycles, settings }: CreateCanisterArgs<u64>,
-    ) -> Result<CreateResult, String> {
-        create_canister128(CreateCanisterArgs {
-            cycles: cycles as u128,
-            settings,
-        })
-            .await
-    }
-
-    async fn create_canister_call(args: CreateCanisterArgs<u128>) -> Result<CreateResult, String> {
-        #[derive(CandidType)]
-        struct In {
-            settings: Option<CanisterSettings>,
+    let (install_res, ): (CreateResult, ) = match api::call::call(
+        Principal::management_canister(),
+        "install_code",
+        (install_config, ),
+    )
+        .await
+    {
+        Ok(x) => x,
+        Err((code, msg)) => {
+            return format!(
+                "An error happened during the call: {}: {}",
+                code as u8, msg
+            );
         }
-        let in_arg = In {
-            settings: Some(normalize_canister_settings(args.settings)?),
-        };
-
-        let (create_result, ): (CreateResult, ) = match api::call::call_with_payment128(
-            Principal::management_canister(),
-            "create_canister",
-            (in_arg, ),
-            args.cycles,
-        )
-            .await
-        {
-            Ok(x) => x,
-            Err((code, msg)) => {
-                return Err(format!(
-                    "An error happened during the call: {}: {}",
-                    code as u8, msg
-                ));
-            }
-        };
-
-        // events::record(events::EventKind::CanisterCreated {
-        //     canister: create_result.canister_id,
-        //     cycles: args.cycles,
-        // });
-        Ok(create_result)
-    }
-
-    // #[update(
-    // guard = "is_custodian_or_controller",
-    // name = "wallet_create_canister128"
-    // )]
-    #[candid_method(update)]
-    #[ic_cdk::update]
-    async fn create_canister128(
-        mut args: CreateCanisterArgs<u128>,
-    ) -> Result<CreateResult, String> {
-        let mut settings = normalize_canister_settings(args.settings)?;
-        let controllers = settings
-            .controllers
-            .get_or_insert_with(|| Vec::with_capacity(2));
-        if controllers.is_empty() {
-            controllers.push(ic_cdk::api::caller());
-            controllers.push(ic_cdk::api::id());
-        }
-        args.settings = settings;
-        let create_result = create_canister_call(args).await?;
-        super::update_chart();
-        Ok(create_result)
-    }
-
-    // Make it so the controller or controllers are stored only in the controllers field.
-    fn normalize_canister_settings(settings: CanisterSettings) -> Result<CanisterSettings, String> {
-        // Agent <= 0.8.0, dfx <= 0.8.1 will send controller
-        // Agents >= 0.9.0, dfx >= 0.8.2 will send controllers
-        // The management canister will accept either controller or controllers, but not both.
-        match (&settings.controller, &settings.controllers) {
-            (Some(_), Some(_)) => {
-                Err("CanisterSettings cannot have both controller and controllers set.".to_string())
-            }
-            (Some(controller), None) => Ok(CanisterSettings {
-                controller: None,
-                controllers: Some(vec![*controller]),
-                ..settings
-            }),
-            _ => Ok(settings),
-        }
-    }
+    };
+    format!("{}", create_result.canister_id)
+}
 
 
-    async fn install_wallet(canister_id: &Principal, wasm_module: Vec<u8>) -> Result<(), String> {
-        // Install Wasm
-        #[derive(CandidType, Deserialize)]
-        enum InstallMode {
-            #[serde(rename = "install")]
-            Install,
-            #[serde(rename = "reinstall")]
-            Reinstall,
-            #[serde(rename = "upgrade")]
-            Upgrade,
+#[cfg(test)]
+mod tests {
+    use std::borrow::Cow;
+    use std::env;
+    use std::fs::{create_dir_all, write};
+    use std::path::Path;
+    use std::path::PathBuf;
+    use std::process::Command;
+
+    use candid::export_service;
+    use ic_cdk::{api, update};
+    use ic_cdk::api::management_canister::main::CanisterSettings;
+    // use ic_cdk::export::candid::{
+    //     candid_method, CandidType, check_prog, Deserialize, export_service, IDLProg, TypeEnv,
+    // };
+    use ic_cdk::export::candid::Principal;
+
+    #[test]
+    fn save_candid_2() {
+        #[ic_cdk_macros::query(name = "__get_candid_interface_tmp_hack")]
+        fn export_candid() -> String {
+            ic_cdk::export::candid::export_service!();
+            __export_service()
         }
 
-        #[derive(CandidType, Deserialize)]
-        struct CanisterInstall {
-            mode: InstallMode,
-            canister_id: Principal,
-            #[serde(with = "serde_bytes")]
-            wasm_module: Vec<u8>,
-            arg: Vec<u8>,
+        let mut dir: PathBuf = env::current_dir().unwrap();
+        let canister_name: Cow<str> = dir.file_name().unwrap().to_string_lossy();
+
+        match create_dir_all(&dir) {
+            Ok(_) => println!("Successfully created directory"),
+            Err(e) => println!("Failed to create directory: {}", e),
         }
 
-        let install_config = CanisterInstall {
-            mode: InstallMode::Install,
-            canister_id: *canister_id,
-            wasm_module: wasm_module.clone(),
-            arg: b" ".to_vec(),
-        };
-
-        match api::call::call(
-            Principal::management_canister(),
-            "install_code",
-            (install_config, ),
-        )
-            .await
-        {
-            Ok(x) => x,
-            Err((code, msg)) => {
-                return Err(format!(
-                    "An error happened during the call: {}: {}",
-                    code as u8, msg
-                ));
-            }
-        };
-
-        // events::record(events::EventKind::WalletDeployed {
-        //     canister: *canister_id,
-        // });
-        #[derive(CandidType, Deserialize)]
-        struct WalletStoreWASMArgs {
-            #[serde(with = "serde_bytes")]
-            wasm_module: Vec<u8>,
-        }
-
-        // Store wallet wasm
-        let store_args = WalletStoreWASMArgs { wasm_module };
-        match api::call::call(*canister_id, "wallet_store_wallet_wasm", (store_args, )).await {
-            Ok(x) => x,
-            Err((code, msg)) => {
-                return Err(format!(
-                    "An error happened during the call: {}: {}",
-                    code as u8, msg
-                ));
-            }
-        };
-        Ok(())
-    }
-
-
-    #[cfg(test)]
-    mod tests {
-        use std::borrow::Cow;
-        use std::env;
-        use std::fs::{create_dir_all, write};
-        use std::path::Path;
-        use std::path::PathBuf;
-        use candid::export_service;
-
-        use ic_cdk::{api, update};
-        use ic_cdk::api::management_canister::main::CanisterSettings;
-        // use ic_cdk::export::candid::{
-        //     candid_method, CandidType, check_prog, Deserialize, export_service, IDLProg, TypeEnv,
-        // };
-        use ic_cdk::export::candid::Principal;
-        use crate::wallet::BalanceResult;
-        use crate::wallet::CreateCanisterArgs;
-        use crate::wallet::CreateResult;
-        // use super::*;
-
-        #[test]
-        fn save_candid_2() {
-            #[ic_cdk_macros::query(name = "__get_candid_interface_tmp_hack")]
-            fn export_candid() -> String {
-                ic_cdk::export::candid::export_service!();
-                __export_service()
-            }
-
-            let dir: PathBuf = env::current_dir().unwrap();
-            let canister_name: Cow<str> = dir.file_name().unwrap().to_string_lossy();
-
-            match create_dir_all(&dir) {
-                Ok(_) => println!("Successfully created directory"),
-                Err(e) => println!("Failed to create directory: {}", e),
-            }
-
-            let res = write(dir.join(format!("{:?}.did", canister_name).replace("\"", "")), export_candid());
-            println!("-------- Wrote to {:?}", dir);
-            println!("-------- res {:?}", canister_name);
-        }
+        let res = write(dir.join(format!("{:?}.did", canister_name).replace("\"", "")), export_candid());
+        println!("-------- Wrote to {:?}", dir);
+        println!("-------- res {:?}", canister_name);
     }
 }
